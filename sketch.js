20e041429e88dde0919f2b1fe259f3fd65ca8916
Valerio Andrini <valerio.andrini@gmail.com>
2018-11-04 16:17:21 +0100

    first effect

--- sketch.js
+++ sketch.js
@@ -1,11 +1,75 @@
-function preload(){
+var mic;
+var happyMonster;
+var angryMonster;
+var monster;
+var posX = 100;
+var posY = 700;
+var morto = false;
+
+function preload() {
   // put preload code here
+  happyMonster = loadImage("./assets/happy_monster.png");
+  angryMonster = loadImage("./assets/angry_monster.png");
+  player = loadImage("./assets/player.png");
+  wall = loadImage("./assets/brick_wall.png");
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+
+  mic = new p5.AudioIn();
+  mic.start();
 }
 
 function draw() {
-  // put drawing code here
+  background('SLATEGREY');
+
+  imageMode(CENTER);
+  // Get the overall volume (between 0 and 1.0)
+  var vol = mic.getLevel();
+
+
+  text(vol, 50, 100);
+
+  if (vol < 0.1 && morto == false) {
+    monster = image(happyMonster, width / 2, height / 2, image.width, image.height);
+
+  } else if (vol > 0.1 && vol < 0.4 && morto == false) {
+    monster = image(happyMonster, width / 2, height / 2, image.width, image.height);
+    posX += vol * 50;
+  } else {
+    monster = image(angryMonster, width / 2, height / 2 - 70, image.width, image.height);
+    push();
+    textSize(22);
+    textAlign(CENTER);
+    text("You LOST, refresh and try again!", width / 6 * 3, height / 6 * 5);
+    pop();
+    morte();
+  }
+  if (posX > 1200) {
+    vittoria();
+  }
+
+  image(player, posX, posY, image.width, image.height);
+
+  push();
+  textSize(22);
+  textAlign(CENTER);
+  text("Escape from the monster, but don't get noticed!", width / 6 * 3, 150);
+  pop();
+}
+
+function morte() {
+  mic.stop();
+  morto = true;
+}
+
+function vittoria() {
+  mic.stop();
+  push();
+  textSize(22);
+  textAlign(CENTER);
+  text("You WON, congrats you escaped the monster!", width / 6 * 3, height / 6 * 5);
+  pop();
+  //text('you won', width / 2, height / 4 * 3);
 }
